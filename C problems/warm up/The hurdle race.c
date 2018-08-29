1e479287752816e978fe7c5a223518c35f66be66
ivvraghavendra <35492336+raghavendraivv@users.noreply.github.com>
2018-08-29 23:53:06 +0530

    #2

---
+++ C problems/warm up/The hurdle race.c
@@ -0,0 +1,40 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n; 
+    int k; 
+    scanf("%d %d",&n,&k);
+    int a[n],i,j,m,p;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]<a[j])
+            {
+                m=a[i];
+                a[i]=a[j];
+                a[j]=m;
+            }
+        }
+    }
+    if(k<a[0])
+    {
+        p=a[0]-k;
+        printf("%d",p);
+    }
+    if(k>a[0])
+    {
+        printf("0");
+    }
+    return 0;
+}
