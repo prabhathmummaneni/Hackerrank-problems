8b6a125110a06467c8fabe8f6a070b1620bc70a7
prabhathmummaneni <43805584+prabhathmummaneni@users.noreply.github.com>
2020-10-01 12:00:57 +0530

    Update Solve me first.c

--- C problems/warm up/Solve me first.c
+++ C problems/warm up/Solve me first.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
 
 
-int solveMeFirst(int a, int b) {
+int so(int a, int b) {
    return a+b;
  
 }
@@ -12,7 +9,7 @@ int main() {
     int num1,num2;
     scanf("%d %d",&num1,&num2);
     int sum; 
-    sum = solveMeFirst(num1,num2);
+    sum = so(num1,num2);
     printf("%d",sum);
     return 0;
 }
